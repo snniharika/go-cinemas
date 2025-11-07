7d589e1cd9b4aea53a4025697c1d9524228db271
prithzzz <prithikasoundararajan@gmail.com>
2025-11-07 12:38:48 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import Navbar from './Companants/Navbar'
-import {Route, Routes} from 'react-router-dom'
+import Navbar from './Componants/Navbar'
+import {Route, Routes, useLocation} from 'react-router-dom'
 import Landing from './Pages/LandingPage'
+import Movies from './Pages/Movies'
+import MovieDetails from './Pages/MovieDetails'
+import SeatSelection from './Pages/SeatSelection'
+import MyBookings from './Pages/MyBookings'
+
 
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Landing/>} />
-        
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+        <Route path='/movies/:id/:date' element={<SeatSelection/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
       </Routes>
     </>
   )
