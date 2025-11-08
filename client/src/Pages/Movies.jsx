1e6e532d39d8488de114084d67cf839493cb96dc
Niharika <snniharika11@gmail.com>
2025-11-08 15:23:54 +0530

    correct Movies.jsx file

---
+++ client/src/Pages/Movies.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {dummyShowsData} from '../assets/assets'
+
+
+const Movies = () => {
+  return dummyShowsData.length > 0 ? (
+    <div className='relative my-40 mb-60 px-6 md:px-16 1g:px-40 x1:px-44 overflow-hidden min-h-[80vh]'>
+        <BlurCircle top="150px" left="0px"/>
+        <BlurCircle bottom="50px" right="50px"/>
+
+
+        <h1 className='text-lg font-medium my-4'> Now showing</h1>
+        <div className='flex flex-wrap max-sm:justify-center gap-8'>
+            {dummyShowsData.map((movie)=> (
+                <MovieCard movie={movie} key={movie._id}/>
+            ))}
+        </div>
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='text-3xl font-bold text-center'> No Movies </h1>
+    </div>
+  )
+}
+
+export default Movies
