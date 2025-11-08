264a8e81d13e0e18bcc9970eb8fb51710fa1e254
prithzzz <prithikasoundararajan@gmail.com>
2025-11-08 18:26:02 +0530

    okay....backend partially done

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+
 
 const app = express();
 const port = 3000;
 
+await connectDB()
+
 //Middleware 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 //API Routes
 app.get('/', (req, res) => res.send('server is live!'))
 
 //Express app
-app.listen(port, () => console.log('server listening at http://localhost:${port}'))
+app.listen(port, () => console.log(`server listening at http://localhost:${port}`))
 
