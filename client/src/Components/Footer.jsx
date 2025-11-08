426966f603d93fd1a717819acaf85a033dbddc96
Niharika <snniharika11@gmail.com>
2025-11-08 15:01:07 +0530

    ok

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className='px-6 md:px-16 lg:px-36 mt-40 w-full text-gray-300'> 
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14" >
+      <p className='pt-4 text-center text-sm pb-5'>
+         Copyright {new Date().getFullYear()} © GoCinemas. All rights reserved.
+      </p>
+      </div>
+    </footer>
   )
 }
 
