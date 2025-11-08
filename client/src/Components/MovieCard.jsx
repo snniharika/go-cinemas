7d87ef2fd8a15b60de141bd9267f734eea870e92
Niharika <snniharika11@gmail.com>
2025-11-08 14:51:44 +0530

    ok

--- client/src/Components/MovieCard.jsx
+++ client/src/Components/MovieCard.jsx
@@ -13,7 +13,7 @@ const MovieCard = ({movie}) => {
 
 
         <p className='text-sm text-gray-400 mt-2'> 
-            {new Date(movie.release_date).getFullYear()} ● {movie.genres.slice(0,2).map(genre=> genre.name).join(" | ")} ● {movie.runtime}
+            {new Date(movie.release_date).getFullYear()} ● {movie.genres.slice(0,2).map(genre=> genre.name).join(" | ")} ● {timeFormat(movie.runtime)}
         </p>
 
         <div className='flex items-center justify-between mt-4 pb-3'>
