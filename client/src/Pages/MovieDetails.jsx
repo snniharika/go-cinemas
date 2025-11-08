8e9da6b55611fa9d683c6fefbf2ede54a78a26fe
Niharika <snniharika11@gmail.com>
2025-11-08 15:53:08 +0530

    ok

---
+++ client/src/Pages/MovieDetails.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {useParams} from 'recat-router-dom'
+import { dummyDateTimeData,dummyShowsData } from '../assets/assets'
+import { StarIcon } from 'lucide-react'
+
+const MovieDetails = () => {
+    const {id}=useParams()
+    const [show,setShow]=useState(null)
+
+    const getShow =async()=> {
+        const show=dummyShowsData.find(show=> show._id ===id)
+        setShow({
+            movie:show,
+            dataTime:dummyDateTimeData
+        })
+    }
+
+useEffect(()=> {
+    getShow()
+},[id])
+
+
+
+
+  return show ? (
+    <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
+        <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
+            <img src={show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover'/>
+
+            <div className='relative flex flex-col gap-3'>
+                <BlurCircle top="100px" left="-100px" />
+                <p className='text-primary'> English</p>
+                <h1 className='text-4xl font-semibold max-w-96 text-balance'> {show.movie.title} </h1>
+                <div className='flex items-center gap-2 text-gray-300'> 
+                    <StarIcon className='w-5 h-5 text-primary fill-primary'/>
+                    {show.movie.vote_average.toFixed(1)} User rating
+                </div>
+
+                <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'> 
+                    {show.movie.overview}
+                </p>
+
+                <p> 
+                    {timeFormat(show.movie.runtime)} ● {show.movie.genres.map(genre=> genre.name).join(",")} ● {show.movie.release_date.split("-")[0]}
+                </p>
+
+                <div>
+                    <button>
+                        <a href="#dateSelect" className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'> Book Now</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+  ) : <div> Loading ..</div>
+}
+
+export default MovieDetails
