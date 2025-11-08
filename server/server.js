2d7624150a23089874887ef936db8ea589bae9f3
prithzzz <prithikasoundararajan@gmail.com>
2025-11-08 18:57:14 +0530

    backend commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 
 const app = express();
@@ -17,6 +19,8 @@ app.use(clerkMiddleware())
 
 //API Routes
 app.get('/', (req, res) => res.send('server is live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
 
 //Express app
 app.listen(port, () => console.log(`server listening at http://localhost:${port}`))
