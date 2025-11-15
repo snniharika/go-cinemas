13bee7795ae4af2501794a9a14155917a2b3a057
prithzzz <prithikasoundararajan@gmail.com>
2025-11-15 11:03:45 +0530

    env updated

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try{
+        const mongoURI = `${process.env.MONGODB_URI}/gocinemas`;
+        console.log('Attempting to connect to:', mongoURI);
+        
         mongoose.connection.on('connected', () => console.log('database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/gocinemas`)
+        mongoose.connection.on('error', (error) => console.log('Connection error:', error));
+        
+        await mongoose.connect(mongoURI);
     } catch(error) {
-        console.log(error.message);
+        console.log('Connection error:', error.message);
     }
 }
 
